fa48e4084f12b2ad02e3bdff86c693e8cff1ef45
zanywhale <zanywhale@gmail.com>
2017-08-11 07:40:20 -0700

    Change indent Tap to Space

--- info_network.hpp
+++ info_network.hpp
@@ -2,18 +2,18 @@
 #define INFO_NETWORK_HPP
 
 class info_network{
-	public:
-		info_network(char INTERFACE[10], char SENDER_IP[16], char TARGET_IP[16]);
-		virtual ~info_network();
-		void find_mac_addr(char MAC_str[13], char user_interface[10]);
-		void print_status();
-	protected:
-	private:
-		char interface[10];
-		char my_MACaddr[13];
-		char my_IPaddr[13];
-		char sender_ip[16];
-		char target_ip[16];
+    public:
+        info_network(char INTERFACE[10], char SENDER_IP[16], char TARGET_IP[16]);
+        virtual ~info_network();
+        void find_mac_addr(char MAC_str[13], char user_interface[10]);
+        void print_status();
+    protected:
+    private:
+        char interface[10];
+        char my_MACaddr[13];
+        char my_IPaddr[13];
+        char sender_ip[16];
+        char target_ip[16];
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 int main(int argc, char *argv[], char *envp[])
 {
-	if(argc != 4) // Have to use four arguments
-	{
-		cout << "\033[1;32mUsage: send_arp <interface> <sender(victim) ip> <target ip>" <<\
-				"\033[0m\n\033[1;31mEx)send_arp wlan0 192.168.10.2 192.168.10.1\033[0m\n";
-		exit(0);
-	}
-	
-	info_network info = info_network(argv[1], argv[2], argv[3]);
-	info.print_status();
-	return 0;
+    if(argc != 4) // Have to use four arguments
+    {
+        cout << "\033[1;32mUsage: send_arp <interface> <sender(victim) ip> <target ip>" <<\
+                "\033[0m\n\033[1;31mEx)send_arp wlan0 192.168.10.2 192.168.10.1\033[0m\n";
+        exit(0);
+    }
+    
+    info_network info = info_network(argv[1], argv[2], argv[3]);
+    info.print_status();
+    return 0;
 }
