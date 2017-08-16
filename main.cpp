1d44fddf5ae53f5425d8425a1e6ce455f143dfc2
zanywhale <zanywhale@gmail.com>
2017-08-16 08:46:53 -0700

    change check_argc()

--- main.cpp
+++ main.cpp
@@ -10,13 +10,15 @@ int main(int argc, char *argv[], char *envp[])
 {
     // start verification // check also interface and ip addr
     // Constructor
-    arp_packet arp(argv[1], argv[2], argv[3]);
-    if(argc != 4)
-        arp.check_argc();
-    else{
-        arp.check_interface(argv[1]);   
-        arp.check_ipaddr(argv[2], argv[3]);
+    if(argc != 4){
+        cout << "\033[1;32mUsage: send_arp <interface> <sender(victim) ip> <target ip> \033[0m" << endl <<\
+                "\033[1;31mEx)send_arp wlan0 192.168.10.2 192.168.10.1\033[0m\n";
+        exit(0);
     }
+    
+    arp_packet arp(argv[1], argv[2], argv[3]);
+    arp.check_interface(argv[1]);   
+    arp.check_ipaddr(argv[2], argv[3]);
     // end verification
 
     // get victim's MAC Address
@@ -27,9 +29,9 @@ int main(int argc, char *argv[], char *envp[])
     arp.print_status();
     
     // infect victim ARP table
-    for(int i = 0; i < 100; i++){
+    for(int i = 0; i < 1000000; i++){
         sleep(1);
-        arp.arp_reply();
+		arp.arp_reply();
     }
 
     return 0;
