418f7736e64ce8ed7d70886f4b3733d84cae504f
zanywhale <zanywhale@gmail.com>
2017-08-15 23:40:26 -0700

    class verification into class info_network

--- info_network.hpp
+++ info_network.hpp
@@ -10,6 +10,9 @@ class info_network{
         virtual ~info_network();
         void find_mac_addr(unsigned char MAC_str[6], char user_interface[10]);
         void print_status();
+        void check_argc();
+        void check_interface(char *interface);
+        void check_ipaddr(char ip1[16], char ip2[16]);
     protected:
         char interface[10];
         unsigned char my_MACaddr[6];
